f4f0e367571c18b7e527c8af11ac0eb32a5c2a60
hossamhaddad <husamhaddad80@gmail.com>
2020-12-27 14:41:25 +0200

    adding css rules

--- pics.js
+++ pics.js
@@ -1,4 +1,6 @@
 var stars=prompt('What star rating would you give our menu, 1-5?');
 for(i=1; i<=stars; i++){
-   document.write('<img src="menu-3.jpg" alt="">')
+   document.write('<p>' + i +  '</p>' + '<img src="menu-3.jpg" alt="" width="200" height="200">')
        }
+    
+       
