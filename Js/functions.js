5b7f5715bf30134a7e1a3fd67bf3e309653d2663
hossamhaddad <husamhaddad80@gmail.com>
2020-12-27 16:33:53 +0200

    fixing js

--- Js/functions.js
+++ Js/functions.js
@@ -22,3 +22,12 @@ if (time<12) {
 }
 alert(message);
 
+
+var stars=prompt('What star rating would you give our menu, 1-5?');
+
+ function userTime(star_rate){
+  for(i=1; i<=star_rate; i++){
+        document.write('<p>' + i +  '</p>' + '<img src="menu-3.jpg" alt="" width="200" height="200">')
+            }
+ }  
+ userTime(stars);
--- Js/pics.js
+++
@@ -1,9 +0,0 @@
-var stars=prompt('What star rating would you give our menu, 1-5?');
-
- function userTime(star_rate){
-  for(i=1; i<=star_rate; i++){
-        document.write('<p>' + i +  '</p>' + '<img src="menu-3.jpg" alt="" width="200" height="200">')
-            }
- }  
- userTime(stars);
-       
