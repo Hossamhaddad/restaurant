8d89e55d4c85074191d57a8b1ecb93fe1e39e3d9
hossamhaddad <husamhaddad80@gmail.com>
2020-12-27 15:56:51 +0200

    adding js

---
+++ Js/functions.js
@@ -0,0 +1,24 @@
+function validateUserInput(){
+    var userInput;
+    while (userInput === '' || userInput === null || userInput === undefined){
+    userInput=prompt('enter your name');}
+    return userInput;
+    }
+
+
+alert('hello dear customer');
+var customerName;
+var time;
+customerName= validateUserInput ();
+
+document.write('welcome  '  + customerName +  ' to our restaurant');
+var message;
+
+var time = prompt('what time is it');
+
+if (time<12) {
+    message='you should check our breakfast meals';
+} else {
+    message='you should check our lunch meals';
+}
+alert(message);
--- pics.js
+++
@@ -1,6 +0,0 @@
-var stars=prompt('What star rating would you give our menu, 1-5?');
-for(i=1; i<=stars; i++){
-   document.write('<p>' + i +  '</p>' + '<img src="menu-3.jpg" alt="" width="200" height="200">')
-       }
-    
-       
